807834690365ce3ac6e667cf534a4fabcc9af36a
Boyan <boboto00o@abv.bg>
2021-05-11 22:55:19 +0300

    new exercises

---
+++ Java-OOP/Interfaces-And-Abstraction/Car-Shop/Car.java
@@ -0,0 +1,15 @@
+package InterfacesAndAbstraction;
+
+public interface Car {
+
+    Integer TIERS = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+
+}
