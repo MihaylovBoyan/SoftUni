811610691755126c15633a26d6d28a2e36f88d3c
Boyan <boboto00o@abv.bg>
2021-05-16 14:13:38 +0300

    New exercisess

---
+++ Java-OOP/Polymorphism/Math-Operation/Main.java
@@ -0,0 +1,12 @@
+package InterfacesAndAbstraction.MathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Math.add(2,2));
+        System.out.println(Math.add(2,2,3));
+        System.out.println(Math.add(2,2,4,1));
+
+
+    }
+}
