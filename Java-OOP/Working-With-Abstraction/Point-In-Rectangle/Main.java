18a734f05b84873adb4923885aadc2d43520a756
Boyan <boboto00o@abv.bg>
2021-07-01 14:30:40 +0300

    First steps in OOP

---
+++ Java-OOP/Working-With-Abstraction/Point-In-Rectangle/Main.java
@@ -0,0 +1,28 @@
+package abstr;
+
+import Input.Reader;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] rectanglePoints = Reader.readArray("\\s+");
+        Point A = new Point(rectanglePoints[0], rectanglePoints[1]);
+        Point B = new Point(rectanglePoints[2], rectanglePoints[3]);
+
+        Rectangle rectangle = new Rectangle(A, B);
+
+        int count = Reader.readArray("\\s+")[0];
+
+        while (count-- > 0) {
+            int[] singlePoint = Reader.readArray("\\s+");
+            Point p = new Point(singlePoint[0], singlePoint[1]);
+
+            boolean isIn = rectangle.contains(p);
+
+            System.out.println(isIn);
+        }
+
+
+    }
+}
---
+++ Java-OOP/Working-With-Abstraction/Point-In-Rectangle/Rectangle.java
@@ -0,0 +1,18 @@
+package abstr;
+
+import Input.Reader;
+
+public class Rectangle {
+    private final Point A;
+    private final Point B;
+
+    public Rectangle(Point A, Point B){
+        this.A = A;
+        this.B = B;
+    }
+
+
+    public boolean contains(Point P) {
+        return P.greaterOrEqual(A) && P.lessOrEqual(B);
+    }
+}
