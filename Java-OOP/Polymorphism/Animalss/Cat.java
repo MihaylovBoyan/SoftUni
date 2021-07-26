3b1b06fd1f6ee13f0a8694cce4efdea3f32d234d
Boyan <boboto00o@abv.bg>
2021-07-26 12:13:43 +0300

    Polymorphism exercises.

---
+++ Java-OOP/Polymorphism/Animalss/Cat.java
@@ -0,0 +1,14 @@
+package Animalss;
+
+public class Cat extends Animal{
+
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s", getName(), getFavouriteFood() + System.lineSeparator() + "MEEOW");
+
+    }
+}
---
+++ Java-OOP/Polymorphism/Animalss/Main.java
@@ -0,0 +1,10 @@
+package Animalss;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
