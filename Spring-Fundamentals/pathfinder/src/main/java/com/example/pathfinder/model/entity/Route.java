29ec1ca36fc1d145434728e0a00dc97e87d7fc18
Boyan <boboto00o@abv.bg>
2021-10-09 19:02:52 +0300

    small project.

---
+++ Spring-Fundamentals/pathfinder/src/main/java/com/example/pathfinder/model/entity/Route.java
@@ -0,0 +1,99 @@
+package com.example.pathfinder.model.entity;
+
+import com.example.pathfinder.model.entity.enums.LevelEnum;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "routes")
+public class Route extends BaseEntity {
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String gpxCoordinates;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private LevelEnum level;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToOne
+    private User author;
+
+    @Column
+    private String videoUrl;
+
+    @ManyToMany
+    private Set<Category> categories;
+
+    public Route() {
+
+    }
+
+    public String getGpxCoordinates() {
+        return gpxCoordinates;
+    }
+
+    public Route setGpxCoordinates(String gpxCoordinates) {
+        this.gpxCoordinates = gpxCoordinates;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Route setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public LevelEnum getLevel() {
+        return level;
+    }
+
+    public Route setLevel(LevelEnum level) {
+        this.level = level;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Route setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Route setAuthor(User author) {
+        this.author = author;
+        return this;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public Route setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+        return this;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public Route setCategories(Set<Category> categories) {
+        this.categories = categories;
+        return this;
+    }
+}
